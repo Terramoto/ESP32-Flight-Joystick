4e905e4dd9af1dede49d593ea5fc868b77c1b5a2
Terramoto <jterramoto@gmail.com>
2020-04-12 19:51:33 +0100

    Added serial support
    Added Serial support for VJoySerialFeed through iBus

--- Joystick.ino
+++ Joystick.ino
@@ -13,18 +13,34 @@
  */
  
 #include <BleGamepad.h> 
+#include "ibus.h"
 
-BleGamepad bleGamepad("Stick do Caralho","João Terramoto",100);
+// How often to send data?
+#define UPDATE_INTERVAL 10 // milliseconds
 
-//Axis Pins
-const short axisPins[] = {34, 36, 38, 39};
-const short pinCount = 4;
+// 1. Analog channels. Data can be read with the Arduino's 10-bit ADC.
+// This gives values from 0 to 1023.
+// Specify below the analog pin numbers (as for analogRead) you would like to use.
+// Every analog input is sent as a single channel.
+#define NUM_ANALOG_INPUTS 4 // Axis
+byte axisPins[]={34,32,33,35}; // element count MUST be == NUM_ANALOG_INPUTS
+#define NUM_DIGITAL_INPUTS 4 // Buttons
+byte buttonPins[]={17,18,19,21}; // Button Pins
+#define ANALOG_REFERENCE EXTERNAL
+
+#define NUM_DIGITAL_BITMAPPED_INPUTS 0
+byte digitalBitmappedPins[] = {8, 9, 10};
+#define NUM_CHANNELS ( (NUM_ANALOG_INPUTS) + (NUM_DIGITAL_INPUTS) + (15 + (NUM_DIGITAL_BITMAPPED_INPUTS))/16 )
+IBus ibus(NUM_CHANNELS);
+
+BleGamepad bleGamepad("Stick do Caralho","Joao Terramoto",100);
+
 const short numberOfPotSamples = 5;     // Number of pot samples to take (to smooth the values)
 const short delayBetweenSamples = 2;    // Delay in milliseconds between pot samples
 const short delayBetweenHIDReports = 5; // Additional delay in milliseconds between HID reports
 
-
 void setup() {
+  //analogReference(ANALOG_REFERENCE);
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   
@@ -33,13 +49,39 @@ void setup() {
   pinMode(18,INPUT_PULLUP);
   pinMode(19,INPUT_PULLUP);
   pinMode(21,INPUT_PULLUP);
+  pinMode(34,INPUT);
+  pinMode(32,INPUT);
+  pinMode(33,INPUT);
+  pinMode(35,INPUT);
   bleGamepad.begin();
 }
 
+//int axisPins[]={34,32,33,35};
+//short buttonPins[]={17,18,19,21};
+short buttonIds[]={1,2,4,8};
+int adjustedValue[] = {0,0,0,0};
+
 void loop() {
   if(bleGamepad.isConnected()){
-    short buttonPins[]={17,18,19,21};
-    short buttonIds[]={1,2,4,8};
+    int potValues[numberOfPotSamples];
+    for(int a=0;a<4;a++)
+    {
+      // Populate readings
+      for(int i = 0 ; i < numberOfPotSamples ; i++)
+      {
+        potValues[i] = analogRead(axisPins[a]);
+        delay(delayBetweenSamples);
+      }
+      
+      int potValue = 0;   // Variable to store calculated pot reading average
+      // Iteraate through the readings to sum the values
+      for(int i = 0 ; i < numberOfPotSamples ; i++)
+      {
+        potValue += potValues[i];
+      }
+      potValue = potValue / numberOfPotSamples;
+      adjustedValue[a] = map(potValue, 0, 4095, 127, -127); 
+    }
     
     for(short a = 0; a<4;a++){
         if(digitalRead(buttonPins[a])==LOW){
@@ -48,45 +90,42 @@ void loop() {
           bleGamepad.release(buttonIds[a]);
         }
     }
-    /*int potValues[numberOfPotSamples];  // Array to store pot readings
-    int adjustedValue[] = {0,0,0,0};// Variable to store calculated pot reading average
-    
-    // Populate readings
-    for(int a = 0; a < pinCount; a++)
-     {
-      for(int i = 0 ; i < numberOfPotSamples ; i++)
-       {
-        potValues[i] = analogRead(axisPins[pinCount]);
-        delay(delayBetweenSamples);
-      }
-      
-      int potValue = 0;
-      
-      // Iteraate through the readings to sum the values
-      for (int i = 0 ; i < numberOfPotSamples ; i++)
-      {
-       potValue += potValues[i]; 
-      }
-      // Calculate the average
-      potValue = potValue / numberOfPotSamples;
-      // Map analog reading from 0 ~ 4095 to 127 ~ -127 for use as an axis reading
-      adjustedValue[a] = map(potValue, 0, 4095, 127, -127);
-    }
-    bleGamepad.setAxes(adjustedValue[0], adjustedValue[1], adjustedValue[2], adjustedValue[3], 0, 0, DPAD_CENTERED);*/
-    short x =map(analogRead(36), 0, 4095, 127, -127);
-    short y =map(analogRead(34), 0, 4095, 127, -127);
-    short z =map(analogRead(35), 0, 4095, 127, -127);
-    short r =map(analogRead(39), 0, 4095, 127, -127);
-    /*Serial.print("X:");
-    Serial.println(analogRead(36));
-    Serial.print("Y:");
-    Serial.println(analogRead(34));
-    Serial.print("Z:");
-    Serial.println(analogRead(35));
-    Serial.print("R:");
-    Serial.println(analogRead(39));*/
-    
-    bleGamepad.setAxes(x, y, z, r, 0, 0, DPAD_CENTERED);
+
+    bleGamepad.setAxes(adjustedValue[0], adjustedValue[1], adjustedValue[2], adjustedValue[3], 0, 0, DPAD_CENTERED);
     delay(delayBetweenHIDReports);
+    //bleGamepad.setBatteryLevel();
+  }else{
+    int i, bm_ch = 0;
+  unsigned long time = millis();
+
+  ibus.begin();
+
+  // read analog pins - one per channel
+  for(i=0; i < NUM_ANALOG_INPUTS; i++)
+    ibus.write(analogRead(axisPins[i]));
+
+  // read digital pins - one per channel
+  for(i=0; i < NUM_DIGITAL_INPUTS; i++)
+    ibus.write(digitalRead(buttonPins[i]) == HIGH ? 1023 : 0);
+
+  // read digital bit-mapped pins - 16 pins go in one channel
+  for(i=0; i < NUM_DIGITAL_BITMAPPED_INPUTS; i++) {
+    int bit = i%16;
+    if(digitalRead(digitalBitmappedPins[i]) == HIGH)
+      bm_ch |= 1 << bit;
+
+    if(bit == 15 || i == NUM_DIGITAL_BITMAPPED_INPUTS-1) {
+      // data for one channel ready
+      ibus.write(bm_ch);
+      bm_ch = 0;
+    }
+  }
+
+  ibus.end();
+
+  time = millis() - time; // time elapsed in reading the inputs
+  if(time < UPDATE_INTERVAL)
+    // sleep till it is time for the next update
+    delay(UPDATE_INTERVAL  - time);
   }
 }
